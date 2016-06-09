a9b26b5ffb9207673cab20fb7628a1bf28389bd7
Arkady Pritsker <arkady.pritsker@cesicorp.com>
2016-06-09 13:45:34 -0400

    Initial Commit.

---
+++ 1-13-2016_My_First_Program.py
@@ -0,0 +1,11 @@
+# This program says hello and makes me feel great.
+
+print('Hello! You are the bees knees.')
+print('What is your name, brah?') # ask for the user's name
+myName = input()
+print('Oh, awesome! Good to meet you, ' + myName)
+print('The length of your name is:')
+print (len(myName))
+print('What is your age guy?') # ask for the user's age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
